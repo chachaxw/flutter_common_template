669b0986def3c11b01e880eb35a7a63261516213
chacha <867571123@qq.com>
2020-11-17 10:02:49 +0800

    feat: add base model class

---
+++ lib/core/base/base_model.dart
@@ -0,0 +1,15 @@
+import 'package:async_redux/async_redux.dart';
+import 'package:flutter/widgets.dart';
+
+abstract class AppBaseModel<St> extends BaseModel<St> {
+  BuildContext context;
+
+  AppBaseModel({
+    this.context,
+    List equals,
+    Widget widget,
+  }) : super(
+          equals: equals ?? [],
+          widget: widget,
+        );
+}
